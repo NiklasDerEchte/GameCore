52f49560be91f7203ab8d65c192cfb41c4ad3920
NiklasDerEchte <30403587+NiklasDerEchte@users.noreply.github.com>
2023-04-21 00:09:06 +0200

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -6,5 +6,4 @@ from core.modules.snowflake_effect import *
 from core.modules.map_gen import *
 from core.modules.debug_fps import *
 from core.modules.projection import *
-from core.modules.test_3d_engine import *
 from core.modules.ai_town import *
